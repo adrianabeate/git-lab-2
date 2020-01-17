cc20cf275bb36d74e536332471867526e54eaba5
adrianabeate <50586703+adrianabeate@users.noreply.github.com>
2020-01-16 22:33:31 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/* 
+* File: git-lab-program.cc 
+* Author: <Adriana Holst> 
+* Date: <1/16/20> 
+* Description: Add Description 
+*/ 
+
+#include <iostream> 
+#include <iomanip> 
+#include <cstdlib> 
+using namespace std; 
+
+
+int main(int argc, char const *argv[]) { 
+cout <<"Hello git!"<< endl;
+/*add code*/ 
+return 0; 
+}// main 
